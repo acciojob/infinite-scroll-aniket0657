7e68f28ce17b8d0ab9c56822531c411a2e2fb454
aniket0657 <117531261+aniket0657@users.noreply.github.com>
2025-03-26 11:59:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+document.addEeventListener("DOMContentLoaded", function(){
+	const list = document.getElementById("Infinite-list");
 
+	function addItem(count = 10) {
+		for (let i = 0;i < count; i++){
+			const item = document.createElement("li");
+			item.textContent = `Item ${list.children.length + 1}`;
+			list.appendChild(item);
+		}
+		
+	}
+	addItem();
+	list.addEventListener("scroll", function () {
+		if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+			addItem(2);
+		}
+	});
+});
